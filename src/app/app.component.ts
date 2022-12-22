174e6f4f1ed0436a5e3a5107c07e52911f2bc4a2
Cobra <conradbravina@gmail.com>
2022-12-22 11:16:00 -0500

    Installed ESLint and prettier and set up the basis of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  accordionComponent!: Array<any>; //Change this to not any
   title = 'AccordionApp';
+  constructor() {
+    this.accordionComponent = [
+      {
+        category: 'Croatian',
+        collapsed: true,
+      },
+    ];
+  }
 }
