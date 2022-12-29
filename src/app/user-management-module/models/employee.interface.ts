cbacdbd77f3610155114338272de4610811bbb9a
Cobra <conradbravina@gmail.com>
2022-12-29 11:27:10 -0500

    Added Employee and gener interface and type respectively

---
+++ src/app/user-management-module/models/employee.interface.ts
@@ -0,0 +1,15 @@
+import { Address } from './address.type';
+
+export interface Employee {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  profileImage: string;
+  birthDate: Date;
+  phone: number;
+  personalSiteUrl: string;
+  about: string;
+  gender: 'male' | 'female';
+  address: Address;
+}
