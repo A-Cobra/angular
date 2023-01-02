1bab3ff53b7fd7af9a4b8871fa0cb1bfd9e289fe
Cobra <conradbravina@gmail.com>
2023-01-02 11:50:09 -0500

    Styling in its final stage

--- src/app/user-management-module/components/not-found/not-found.component.ts
+++ src/app/user-management-module/components/not-found/not-found.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   template: `
-    <h1 class="error">ERROR 404</h1>
-    <h2>Sorry, the resource you've been searching for, doesn't exist</h2>
-    <a routerLink="">Go Home</a>
+    <div>
+      <h1 class="error">ERROR 404</h1>
+      <h2>Sorry, the resource you've been searching for, doesn't exist</h2>
+      <a routerLink="">Go Home</a>
+    </div>
   `,
   styleUrls: ['./not-found.component.scss'],
 })
