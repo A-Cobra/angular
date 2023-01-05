d912585673462d3437e527a1f1825c4ed19ee03b
Cobra <conradbravina@gmail.com>
2023-01-05 11:40:37 -0500

    The form and its event emitter work

--- src/app/github-app-module/containers/search-form/search-form.component.ts
+++ src/app/github-app-module/containers/search-form/search-form.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormArray, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormArray,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { FormEvent } from '../../models/form-event.type';
 
 @Component({
   selector: 'app-search-form',
   template: `
+    <div>OK</div>
     <div>
       <form [formGroup]="fetchForm">
         <input
           formControlName="input"
           type="text"
           placeholder="Enter Github user name" />
-        <button>Search</button>
+        <button (click)="onSearch()">Search</button>
       </form>
     </div>
     <pre>{{ fetchForm.value | json }}</pre>
   `,
   styleUrls: ['./search-form.component.scss'],
 })
-export class SearchFormComponent {
-  fetchForm = new FormGroup({ input: new FormControl('') });
-  constructor() {}
+export class SearchFormComponent implements OnInit {
+  @Output()
+  formEvent = new EventEmitter<FormEvent>();
+  fetchForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.fetchForm = this.formBuilder.group({
+      input: new FormControl<string>('123', {
+        nonNullable: true,
+        validators: [Validators.required],
+        asyncValidators: [],
+      }),
+    });
+  }
+  onSearch() {
+    this.formEvent.emit({
+      type: 'search',
+      inputValue: this.fetchForm.get('input')?.value,
+    });
+  }
 }
