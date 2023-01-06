c36282d33a120eee495483138c6f3e18eda6ce3c
Cobra <conradbravina@gmail.com>
2023-01-06 18:28:37 -0500

    added some styling to the information and reposiories

--- src/app/github-app-module/services/github-profile-fetcher/github-profile-fetcher.service.ts
+++ src/app/github-app-module/services/github-profile-fetcher/github-profile-fetcher.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { GithubUser } from '../../models/github-user.type';
 import { Repository } from '../../models/repository.type';
-import { catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,18 +11,26 @@ import { catchError, of } from 'rxjs';
 export class GithubProfileFetcherService {
   githubEndpointBase = environment.githubEndpoint;
   constructor(private http: HttpClient) {}
+  headers = new HttpHeaders({
+    Authorization: `Bearer ${environment.authToken}`,
+    Accept: 'application/vnd.github+json',
+  });
   getUser(username: string) {
-    return this.http.get<GithubUser>(`${this.githubEndpointBase}/${username}`);
+    return this.http.get<GithubUser>(`${this.githubEndpointBase}/${username}`, {
+      headers: this.headers,
+    });
   }
   getFollowers(username: string) {
     return this.http.get<GithubUser[]>(
-      `${this.githubEndpointBase}/${username}/followers${environment.followersUrlParams}`
+      `${this.githubEndpointBase}/${username}/followers${environment.followersUrlParams}`,
+      { headers: this.headers }
     );
     // .pipe(catchError(error => of([])));
   }
   getRepositories(username: string) {
     return this.http.get<Repository[]>(
-      `${this.githubEndpointBase}/${username}/repos${environment.repositoriesUrlParams}`
+      `${this.githubEndpointBase}/${username}/repos${environment.repositoriesUrlParams}`,
+      { headers: this.headers }
     );
     // .pipe(catchError(error => of([])));
   }
