4720c29631dcb11df50f99c516b3b8c7ab424632
Cobra <conradbravina@gmail.com>
2023-01-16 10:46:13 -0500

    Enhanced the cart ui

--- src/app/fast-food-module/components/menu-selection-header/menu-selection-header.component.ts
+++ src/app/fast-food-module/components/menu-selection-header/menu-selection-header.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { defaultMenuSelection } from 'src/app/utils/default-menu-selection';
 import { MenuItem } from '../../models/menu-item.interface';
 
@@ -6,32 +12,79 @@ import { MenuItem } from '../../models/menu-item.interface';
   selector: 'app-menu-selection-header',
   styleUrls: ['./menu-selection-header.component.scss'],
   template: `
-    <section class="flex-container">
-      <div *ngIf="menuItem.image" class="img-container">
-        <img [src]="menuItem.image" [alt]="menuItem.name" />
-      </div>
-      <article>
-        <div class="text">
-          <div class="main-info">
-            <h3>{{ menuItem.name }}</h3>
-            <p>
-              Base price:
-              <span>{{
-                menuItem.basePrice | currency : 'USD' : 'symbol' : '1.2-2'
-              }}</span>
-            </p>
+    <div class="menu-selection-header-container">
+      <!-- <header *ngIf="cartHeader" class="flex">
+        <h3>
+          Total Price:
+          <span>{{ totalPrice | currency : 'USD' : 'symbol' : '1.2-2' }}</span>
+        </h3>
+        <button (click)="onCompleteOrder()">Add to cart</button>
+      </header> -->
+      <section class="flex-container">
+        <div *ngIf="menuItem.image" class="img-container">
+          <img [src]="menuItem.image" [alt]="menuItem.name" />
+        </div>
+        <article>
+          <div class="text">
+            <div class="main-info">
+              <h3>{{ menuItem.name }}</h3>
+              <p *ngIf="cartHeader; else basePrice">
+                Partial Price:
+                <span>
+                  {{ totalPrice | currency : 'USD' : 'symbol' : '1.2-2' }}
+                </span>
+              </p>
+              <ng-template #basePrice>
+                <p>
+                  Base price:
+                  <span>{{
+                    menuItem.basePrice | currency : 'USD' : 'symbol' : '1.2-2'
+                  }}</span>
+                </p>
+              </ng-template>
+            </div>
+            <div class="description">{{ menuItem.description }}</div>
           </div>
-          <div class="description">{{ menuItem.description }}</div>
-        </div>
-        <div class="icon">
-          <i class="fa-solid fa-angle-right"></i>
-        </div>
-      </article>
-    </section>
+          <div class="icon">
+            <i class="fa-solid fa-angle-right"></i>
+          </div>
+        </article>
+      </section>
+    </div>
   `,
 })
 export class MenuSelectionHeaderComponent {
   @Input()
+  totalPrice: number = 0;
+  @Input()
+  cartHeader: boolean = false;
+  @Input()
   menuItem: MenuItem = { ...defaultMenuSelection };
-  constructor() {}
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
+  // ngAfterViewInit(): void {
+  //   this.calculatePrice();
+  //   this.changeDetector.detectChanges();
+  // }
+
+  calculatePrice() {
+    console.log('Recalculating price');
+    let recalculatedPrice = this.menuItem.basePrice;
+    console.log('recalculatedPrice base price');
+    console.log(recalculatedPrice);
+    for (const customizableOption of this.menuItem.customizableOptions) {
+      if (customizableOption?.options) {
+        for (const option of customizableOption?.options) {
+          if (option.selected) {
+            recalculatedPrice += option.extraPrice ?? 0;
+          }
+        }
+      }
+    }
+    this.totalPrice = recalculatedPrice;
+  }
+
+  onCompleteOrder() {
+    console.log('Order added');
+  }
 }
--- src/app/fast-food-module/containers/cart-preview-menu/cart-preview-menu.component.ts
+++ src/app/fast-food-module/containers/cart-preview-menu/cart-preview-menu.component.ts
@@ -12,12 +12,48 @@ export class CartPreviewMenuComponent implements OnInit {
   previouslySelectedItemId = -1;
   cartMenu: MenuItem[] = [];
   constructor(private cartService: CartService, private router: Router) {}
+  totalItemsPrice: number[] = [];
+  totalCartPrice: number = 0;
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe({
       next: (menu: MenuItem[]) => {
         this.cartMenu = menu;
       },
+      complete: () => {
+        this.cartMenu.forEach((cartMenuItem: MenuItem) => {
+          this.totalItemsPrice.push(this.calculatePrice(cartMenuItem));
+        });
+        this.totalCartPrice = this.totalItemsPrice.reduce(
+          (item1, item2) => item1 + item2,
+          0
+        );
+      },
     });
+  }
+
+  calculatePrice(menuItem: MenuItem) {
+    console.log('Recalculating price');
+    let recalculatedPrice = menuItem.basePrice;
+    console.log('recalculatedPrice base price');
+    console.log(recalculatedPrice);
+    for (const customizableOption of menuItem.customizableOptions) {
+      if (customizableOption?.options) {
+        for (const option of customizableOption?.options) {
+          if (option.selected) {
+            recalculatedPrice += option.extraPrice ?? 0;
+          }
+        }
+      }
+    }
+    return recalculatedPrice;
+  }
+
+  onCardClick() {
+    console.log('Creating form');
+  }
+
+  onCompleteOrder() {
+    console.log('Completing order');
   }
 }
