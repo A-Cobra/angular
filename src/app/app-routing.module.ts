4ba6e0eb747822d1239bbee7d7e4e330265ebaf9
Cobra <conradbravina@gmail.com>
2023-01-22 17:34:44 -0500

    Added the not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./grocery/grocery.module').then(m => m.GroceryModule),
   },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('../app/not-found/not-found.module').then(m => m.NotFoundModule),
+  },
 ];
 
 @NgModule({
