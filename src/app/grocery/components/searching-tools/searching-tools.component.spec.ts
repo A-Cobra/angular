4bd3c3f5dac28ea933f141acb0179b0f955bd642
Cobra <conradbravina@gmail.com>
2023-01-30 07:16:14 -0500

    Almost finished with the SearchingToolsComponent

--- src/app/grocery/components/searching-tools/searching-tools.component.spec.ts
+++ src/app/grocery/components/searching-tools/searching-tools.component.spec.ts
@@ -32,14 +32,25 @@ describe('SearchingToolsComponent Tests', () => {
       expect(component.categories).not.toBe(undefined);
       expect(component.categories.length).toBe(30);
     });
+  });
+
+  describe('HTML or UI Tests', () => {
     test('that changing a category with the selector emits an EventEmitter', () => {
+      jest.spyOn(component.searchingToolsUsage, 'emit');
       const selectInput = debugElement.query(By.css('#category-selector'));
-      console.log('selectInput.nativeElement.value');
-      console.log(
-        selectInput.nativeElement.value === ''
-          ? 'All'
-          : selectInput.nativeElement.value
-      );
+      selectInput.nativeElement.value = 'computers';
+      selectInput.nativeElement.dispatchEvent(new Event('change'));
+      expect(component.searchingToolsUsage.emit).toHaveBeenCalledTimes(1);
+    });
+    test('that making an input an waiting for 1 sec trigger the searching tools EventEmitter', () => {
+      jest.useFakeTimers();
+      jest.spyOn(component.searchingToolsUsage, 'emit');
+      const productNameInput = debugElement.query(By.css('#searchInput'));
+      productNameInput.nativeElement.value = 'justin';
+      productNameInput.triggerEventHandler('input');
+      expect(component.searchingToolsUsage.emit).toHaveBeenCalledTimes(0);
+      jest.advanceTimersByTime(1000);
+      expect(component.searchingToolsUsage.emit).toHaveBeenCalledTimes(1);
     });
   });
 });
